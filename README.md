399430a1b87173762f4bfb77b107a900bf445dc9
Nick <zhong.ren@hotmail.com>
2023-11-17 12:14:30 -0500

    Added CSV format output and removed TSV output

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Human Gene Pathway
 
-This repository processes and combines human gene-pathway data from PathwayCommons and WikiPathways, creating a unified dataset for genomic analysis.
+This repository processes and combines human gene-pathway data from PathwayCommons and WikiPathways. It focuses on the associations between human genes and various biological pathways sourced from these platforms, creating a unified dataset for genomic analysis.
 
 ### Execution
 
@@ -18,14 +18,19 @@ python process.py
 
 - wikipathways-Homo_sapiens.gmt
   - Downloaded from the WikiPathways database, this file contains curated biological pathways for Homo sapiens (humans). Each entry includes pathway information such as gene sets associated with specific biological processes or diseases.
+  - WikiPathways is an open-source platform that offers manually curated biological pathways. 
 - PathwayCommons12.All.hgnc.gmt
-  - This file from Pathway Commons provides comprehensive pathway data, including gene interactions and pathway information. 
+  - This file from Pathway Commons provides comprehensive pathway data, including gene interactions and pathway information. - PathwayCommons is a comprehensive collection of pathways from multiple databases. 
 - Homo_sapiens.gene_info.gz
   - The file from NCBI is a compressed archive containing detailed information on genes.
 
 ### Output
-- human-gene-pathway.tsv
-  - The file contains combined human gene-pathway data from PathwayCommons and WikiPathways.
+- node_Pathway.csv.gz
+  - This file lists all the pathways with their identifiers, names, and URLs.
+- edge_Gene_participatesIn_Pathway.csv.gz
+  - This file describes the associations between genes and pathways.
+
+Note: These CSV files are formatted for easy import into Neo4j.
 
 ## License
 
--- process.py
+++ process.py
@@ -2,36 +2,38 @@ import csv
 import pandas as pd
 import gzip
 from typing import Dict
+from pathlib import Path
 
-def read_gmt(path):
-    if path.endswith('.gz'):
-        open_func = gzip.open
-    else:
-        open_func = open
+def read_gmt(path: str):
+    open_func = gzip.open if path.endswith('.gz') else open
 
     with open_func(path, 'rt') as read_file:
         return [(row[0], row[1], set(row[2:])) for row in csv.reader(read_file, delimiter='\t') if row]
 
-def parse_description(description):
+def parse_description(description: str):
     return dict(item.split(': ', 1) for item in description.split('; ') if ': ' in item)
 
-def process_pathwaycommons_data(path: str, symbol_to_entrez: Dict[str, int]) -> pd.DataFrame:
+def process_pathway_data(path: str, symbol_to_entrez: Dict[str, int]) -> pd.DataFrame:
     data = []
+
     for url, description, genes in read_gmt(path):
         filtered_genes = {symbol_to_entrez.get(gene) for gene in genes} - {None}
+
         if any(gene in symbol_to_entrez for gene in genes):
             data.append({
-                'identifier': url.rsplit('/', 1)[-1],
+                'identifier': Path(url).stem,
                 'name': parse_description(description).get('name'),
                 'url': url,
                 'genes': filtered_genes
             })
+
     return pd.DataFrame(data)
 
 def process_wikipathways_data(gmt_data, human_genes: set):
     wikipath_df = pd.DataFrame(gmt_data, columns=['name', 'description', 'genes'])
     wikipath_df['name'] = wikipath_df['name'].str.split('%').str[0]
     wikipath_df['genes'] = wikipath_df['genes'].apply(lambda genes: {int(gene) for gene in genes} & human_genes)
+    
     return wikipath_df[wikipath_df['genes'].astype(bool)]
 
 def create_combined_df(pc_df, wikipath_df):
@@ -39,28 +41,45 @@ def create_combined_df(pc_df, wikipath_df):
         identifier=wikipath_df['description'].str.rsplit('/', n=1).str[-1],
         url=wikipath_df['description']
     )
+
     combined_df = pd.concat([wikipath_df, pc_df], ignore_index=True).drop_duplicates('genes')
     combined_df['n_genes'] = combined_df['genes'].apply(len)
+    
     return combined_df[['identifier', 'name', 'url', 'n_genes', 'genes']].sort_values('identifier')
 
-def write_to_tsv(dataframe, filepath):
-    dataframe['genes'] = dataframe['genes'].apply(lambda genes_set: '|'.join(map(str, sorted(map(int, genes_set)))))
-    dataframe.to_csv(filepath, index=False, sep='\t')
+def write_to_node_csv(dataframe: pd.DataFrame, filepath: str) -> None:
+    dataframe[['identifier', 'name', 'url']].to_csv(filepath, index=False)
+
+def write_to_edge_csv(dataframe: pd.DataFrame, filepath: str) -> None:
+    edge_data = []
+
+    for _, row in dataframe.iterrows():
+        pathway_id = row['identifier']
+        genes = row['genes'].split('|')
+
+        for gene_id in genes:
+            edge_data.append({'source_id': gene_id, 'target_id': pathway_id})
+
+    edges_df = pd.DataFrame(edge_data)
+    edges_df.to_csv(filepath, index=False)
 
 def main():
     gene_info_path = 'data/input/Homo_sapiens.gene_info.gz'
+    
     with gzip.open(gene_info_path, 'rt') as file:
         gene_df = pd.read_csv(file, delimiter='\t')
 
     human_genes = set(gene_df['GeneID'])
     symbol_to_entrez = dict(zip(gene_df['Symbol'], gene_df['GeneID']))
 
-    pc_df = process_pathwaycommons_data("data/input/PathwayCommons12.All.hgnc.gmt.gz", symbol_to_entrez)
+    pc_df = process_pathway_data("data/input/PathwayCommons12.All.hgnc.gmt.gz", symbol_to_entrez)
     gmt_data = read_gmt('data/input/wikipathways-Homo_sapiens.gmt')
     wikipath_df = process_wikipathways_data(gmt_data, human_genes)
 
     pathway_df = create_combined_df(pc_df, wikipath_df)
-    write_to_tsv(pathway_df, 'data/output/human-gene-pathway.tsv')
+    
+    write_to_node_csv(pathway_df, 'data/output/node_Pathway.csv.gz')
+    write_to_edge_csv(pathway_df, 'data/output/edge_Gene_participatesIn_Pathway.csv.gz')
 
 if __name__ == "__main__":
     main()
